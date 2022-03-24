587372df7231cca154bd3060ab7762c8dcf20cb5
KirillKhoroshilov <kirill.khoroshilov@qligent.com>
2022-03-24 11:34:13 +0300

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 import gulpPackage from "gulp";
 import typescriptCompiler from "gulp-typescript";
 import modifyCompiler from "gulp-modify-file";
+import renameCompiler from "gulp-rename";
 
 const { src, dest, series } = gulpPackage;
 
@@ -9,8 +10,10 @@ const typescriptProject = typescriptCompiler.createProject("tsconfig.json");
 const compileTypescript = () =>
   src("./lib/**/*.ts")
     .pipe(
-      modifyCompiler((content, path, file) =>
-        content.replace('"./proofOfWorkWorker.ts"', '"./proofOfWorkWorker.js"')
+      modifyCompiler((content) =>
+        content
+          .replace('"./worker.js"', '"./worker.cjs"')
+          .replace('"./proofOfWorkWorker.ts"', '"./proofOfWorkWorker.js"')
       )
     )
     .pipe(typescriptProject())
@@ -20,9 +23,15 @@ const moveWorker = () =>
   src("./lib/block/worker.js")
     .pipe(
       modifyCompiler((content) =>
-        content.replace('require("ts-node").register();', "")
+        content
+          .replace('require("ts-node").register();', "")
+          .replace(
+            "require(path.resolve(__dirname, workerData.path));",
+            "import(path.resolve(__dirname, workerData.path));"
+          )
       )
     )
+    .pipe(renameCompiler((path) => (path.extname = ".cjs")))
     .pipe(dest("./build/block"));
 
 export const build = series(compileTypescript, moveWorker);
